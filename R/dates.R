923d2dd5df1ce009273923cffc791813b93bc544
Robert Wilson <robertwilson190@gmail.com>
2019-01-11 11:12:25 +0000

    simple change to make calc more efficient

--- R/dates.R
+++ R/dates.R
@@ -11,16 +11,20 @@
 
 nc_dates <- function(ff) {
 
+
+	if(!file_valid(ff))
+		stop(stringr::str_glue("error: {ff} does not exist or is not netcdf"))
+
 	ff_dates <<- system(stringr::str_c("cdo showtimestamp ", ff), intern = TRUE, ignore.stderr = TRUE) %>%
 		stringr::str_split(" ") %>%
 		.[[1]]
 	ff_dates <- ff_dates[ff_dates != ""]
 	ff_dates <- as.Date(ff_dates)
 	dplyr::data_frame(Date = ff_dates) %>%
+		dplyr::distinct() %>%
 		dplyr::mutate(Year = lubridate::year(Date)) %>%
 		dplyr::mutate(Month = lubridate::month(Date)) %>%
-		dplyr::mutate(Day = lubridate::day(Date)) %>%
-		dplyr::distinct()
+		dplyr::mutate(Day = lubridate::day(Date))
 
 }
 
