eb62ef8b837d45026d1c3cc792b7cf41c67e9993
Robert Wilson <robertwilson190@gmail.com>
2019-02-18 11:08:28 +0000

    add example to nc_format

--- R/format.R
+++ R/format.R
@@ -6,14 +6,13 @@
 #' @param ff This is the file to check.
 #' @export
 
+#'@examples
+
+#' # Get the format of the sample file
+#' ff <- system.file("extdata", "woa18_decav_t01_01.nc", package = "rcdo")
+#' nc_format(ff)
 
 nc_format <- function(ff) {
-
-	result <- system(stringr::str_c("cdo showformat ", ff), intern = TRUE, ignore.stderr = TRUE)
-	result
-
-
+  result <- system(stringr::str_c("cdo showformat ", ff), intern = TRUE, ignore.stderr = TRUE)
+  result
 }
-
-
-
