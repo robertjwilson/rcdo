667b97b6692ad945e90fd5ec20636fd80258bcd5
Robert Wilson <robertwilson190@gmail.com>
2019-02-18 11:51:21 +0000

    add example for nc_variables

--- R/names.R
+++ R/names.R
@@ -1,4 +1,6 @@
 
+# to do:
+# this really should be a data frame giving the units, full descriptions etc.
 
 
 #' @title nc_variables
@@ -7,9 +9,16 @@
 #' @param ff This is the file to check. This must be the full system path to the file.
 #' @export
 
-# need an option for cacheing results...
+
+#' # Get the variable names in the NOAA WOA sample file
+#' ff <- system.file("extdata", "woa18_decav_t01_01.nc", package = "rcdo")
+#' nc_variables(ff)
+#'
 
 nc_variables <- function(ff) {
+
+	if(!file_valid(ff))
+		stop(stringr::str_glue("error: {ff} does not exist or is not netcdf"))
 
 	ff_names <<- system(stringr::str_c("cdo showname ", ff), intern = TRUE, ignore.stderr = TRUE) %>%
 		stringr::str_split(" ") %>%
