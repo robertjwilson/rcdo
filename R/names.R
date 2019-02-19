988fc8245c8a4382021994cc2b938af5c6d77336
Robert Wilson <robertwilson190@gmail.com>
2019-02-19 12:59:40 +0000

    add a more sensible title for nc_variables

--- R/names.R
+++ R/names.R
@@ -3,7 +3,7 @@
 # this really should be a data frame giving the units, full descriptions etc.
 
 
-#' @title nc_variables
+#' @title List the variables in a netcdf file.
 #'
 #' @description This function will tell you the variables in a netcdf file
 #' @param ff This is the file to check. This must be the full system path to the file.
