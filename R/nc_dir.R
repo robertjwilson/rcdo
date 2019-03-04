6725476a5095a417ce2747d89b065950e2b31908
Robert Wilson <robertwilson190@gmail.com>
2019-03-04 10:48:34 +0000

    add print argument for nc_dir

--- R/nc_dir.R
+++ R/nc_dir.R
@@ -4,10 +4,12 @@
 #'
 #' @description This function will list the netcdf files in a directory that contain a specified variable
 #' @param directory This is the directory to check.
+#' @param recursive Do you want to recursive look through a directory?
+#' @param print Do you want to print the name of files as you check them?
 #' @return data frame. First column is the file name. Remaining column are the variables, with TRUE signifying the variable is available in the file on that row.
 #' @export
 
-nc_dir <- function(directory = getwd(), recursive = TRUE) {
+nc_dir <- function(directory = getwd(), recursive = TRUE, print = FALSE) {
 	setwd(directory)
 	on.exit(setwd(getwd()))
 
@@ -16,6 +18,8 @@ nc_dir <- function(directory = getwd(), recursive = TRUE) {
 		tibble::enframe(name = NULL)
 
 	get_vars <- function(ff){
+		if(print)
+			print(stringr::str_glue("getting variables from {ff}"))
 		nc_variables(ff)
 
 	}
