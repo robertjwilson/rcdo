225247aaee5e9f82d7be8415e655969ee9130d49
Robert Wilson <robertwilson190@gmail.com>
2019-03-07 15:40:26 +0000

    add github synch tests code

--- tests/run_before_github_sync.R
+++ tests/run_before_github_sync.R
@@ -8,6 +8,15 @@ uk_coords <- expand.grid(Longitude = seq(-20, 10, 1), Latitude = seq(48, 62, 1))
 ff <- system.file("extdata", "woa18_decav_t01_01.nc", package = "rcdo")
 nc_cellareas(ff)
 
+# clip to a specific depth range using vert_range
+
+nc_clip(ff, vert_range = c(1, 5))
+
+# clip to a specific longitude and latitude box
+# Clipping to the region around the UK
+
+uk_sst <- nc_clip(ff, lon_range = c(-12, 10), lat_range = c(48, 62))
+
 nc_dates(ff)
 
 nc_depths(ff)
