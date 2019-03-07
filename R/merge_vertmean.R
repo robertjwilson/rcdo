8513c8881f6a92913100598466d62c6650de6d44
Robert Wilson <robertwilson190@gmail.com>
2019-03-07 15:10:47 +0000

    switch to using unlink for removing temporary files/folder

--- R/merge_vertmean.R
+++ R/merge_vertmean.R
@@ -197,7 +197,11 @@ nc_merge_vertmean <- function(ff_list, vars = NULL, coords = NULL, vert_scale, m
 
   setwd(init_dir)
   file.copy(stringr::str_c(temp_dir, "/merged.nc"), out_file, overwrite = overwrite)
-  if (file.exists(stringr::str_c(temp_dir, "/merged.nc"))) {
-    file.remove(stringr::str_c(temp_dir, "/merged.nc"))
-  }
+
+  # remove the temporary files created
+  setwd(temp_dir)
+  if(length(dir(temp_dir)) < 6 & temp_dir != init_dir)
+  	unlink(temp_dir, recursive = TRUE)
+
+
 }
