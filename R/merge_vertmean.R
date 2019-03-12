3bb0f63e5823613d74ad4948e8a2506b887d6b08
Robert Wilson <robertwilson190@gmail.com>
2019-03-12 16:09:53 +0000

    add "/" to temp_dir when copying files to temp directory

--- R/merge_vertmean.R
+++ R/merge_vertmean.R
@@ -60,7 +60,7 @@ nc_merge_vertmean <- function(ff_list, vars = NULL, coords = NULL, vert_scale, m
     ens_file <- stringr::str_glue("raw{tracker}.nc")
     new_ens <- c(new_ens, ens_file)
     tracker <- tracker + 1
-    file.copy(ff, stringr::str_c(temp_dir, ens_file), overwrite = TRUE)
+    file.copy(ff, stringr::str_c(temp_dir, ",", ens_file), overwrite = TRUE)
   }
 
   setwd(temp_dir)
