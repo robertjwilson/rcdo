9233433abeb411c1c4cd859074f394d2512e1cc3
Robert Wilson <robertwilson190@gmail.com>
2019-03-20 12:26:37 +0000

    add second example to documentation

--- R/variables.R
+++ R/variables.R
@@ -1,6 +1,4 @@
 
-# to do:
-# this really should be a data frame giving the units, full descriptions etc.
 #' @title List the variables in a netcdf file.
 #' @description This function will tell you the variables in a netcdf file.
 #' @param ff This is the file to read variables from.
@@ -11,6 +9,8 @@
 #' # Get the variable names in the NOAA WOA sample file
 #' ff <- system.file("extdata", "woa18_decav_t01_01.nc", package = "rcdo")
 #' nc_variables(ff)
+#' # A data frame showing the variables, their long names and units
+#' nc_variables(ff, details = TRUE)
 nc_variables <- function(ff, detailed = FALSE) {
   if (!file_valid(ff)) {
     stop(stringr::str_glue("error: {ff} does not exist or is not netcdf"))
@@ -25,22 +25,16 @@ nc_variables <- function(ff, detailed = FALSE) {
   # now, we need to pull in the details
 
   if(detailed){
-
   nc <- ncdf4::nc_open(ff)
   df_var <- list()
   for(i in 1:length(ff_names)){
   	i_name <- ncdf4::ncatt_get(nc, ff_names[i])$long_name
   	i_units <- ncdf4::ncatt_get(nc, ff_names[i])$units
-
-  	df_var[[i]] <- tibble(variable = ff_names[i], long_name = i_name, units = i_units)
+  	df_var[[i]] <- tibble::tibble(variable = ff_names[i], long_name = i_name, units = i_units)
   }
   ncdf4::nc_close(nc)
   result <- df_var %>%
   	dplyr::bind_rows()
-
   }
-
   result
-
-
 }
