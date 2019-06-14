777541c569cfaea51bc7b5bbcf3294640be3c231
r4ecology <robertwilson190@gmail.com>
2019-06-14 12:12:09 +0100

    delete empty line

--- R/zip.R
+++ R/zip.R
@@ -18,6 +18,7 @@ nc_zip <- function(ff, out_file = ff, overwrite = FALSE) {
   # create a temp file for the cdo zipping process
   dummy_zip <- stringr::str_c(tempfile(), ".nc")
 
+
   # zip the file
   system(stringr::str_glue("cdo -f nc4 -z zip_9 copy {ff} {dummy_zip}"), ignore.stdout = TRUE, ignore.stderr = TRUE)
   file.rename(dummy_zip, out_file)
